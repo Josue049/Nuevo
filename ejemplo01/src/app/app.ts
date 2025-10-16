df02c5462067a7734cad6bada18b11c7ef475d6e
Josué Ángel Gutiérrez Cuéllar <77085961+Josue049@users.noreply.github.com>
2025-10-16 14:29:13 +0000

    nuevo

--- ejemplo01/src/app/app.ts
+++ ejemplo01/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ejemplo01');
+  nombre: string="Josue";
 }
---
+++ ejemplo01/src/app/mi-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-componente',
+  templateUrl: './mi-componente.component.html',
+  styleUrls: ['./mi-componente.component.css']
+})
+export class MiComponenteComponent {
+  mensaje: string = '¡Hola, soy un componente de Angular!';
+
+  constructor() { }
+}
